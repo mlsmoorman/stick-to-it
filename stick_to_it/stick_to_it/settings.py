aa9ba241cfb6fd56ee86ac024347c1893f9125e1
mlsmoorman <melissa.s.moorman@gmail.com>
2023-12-15 13:15:31 -0600

    adds database

--- stick_to_it/stick_to_it/settings.py
+++ stick_to_it/stick_to_it/settings.py
@@ -76,8 +76,8 @@ WSGI_APPLICATION = 'stick_to_it.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stick_to_it',
     }
 }
 
