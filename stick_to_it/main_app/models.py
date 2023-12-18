1cf03a04cdd066d7804c526cfd6c48a2ee04951a
mlsmoorman <melissa.s.moorman@gmail.com>
2023-12-18 10:06:17 -0600

    adds create

--- stick_to_it/main_app/models.py
+++ stick_to_it/main_app/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Card(models.Model):
     activity = models.CharField(max_length=100)
     due_date = models.DateField('Due Date')
     complete_date = models.DateField('Complete Date')
+    
+    def get_absolute_url(self):
+        return reverse('index')
     
--- stick_to_it/main_app/urls.py
+++ stick_to_it/main_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('cards/', views.cards_index, name='index'),
+  path('cards/<int:card_id>/', views.cards_detail, name='detail'),
+  path('cards/create', views.CardCreate.as_view(), name='card_create'),
 ]
