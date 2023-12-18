03ee5fa628a10d2f6624f0adef7849637fc3f693
mlsmoorman <melissa.s.moorman@gmail.com>
2023-12-18 10:30:07 -0600

    updates model to have optional dates

--- stick_to_it/main_app/models.py
+++ stick_to_it/main_app/models.py
@@ -4,8 +4,8 @@ from django.urls import reverse
 # Create your models here.
 class Card(models.Model):
     activity = models.CharField(max_length=100)
-    due_date = models.DateField('Due Date')
-    complete_date = models.DateField('Complete Date')
+    due_date = models.DateField(blank=True)
+    complete_date = models.DateField(blank=True)
     
     def get_absolute_url(self):
         return reverse('index')
