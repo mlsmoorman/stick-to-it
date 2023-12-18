05dc2f99cb1898c33fcd0eb0d38af45595ae224b
nathanaelchojnacki <nbumpo11@gmail.com>
2023-12-18 11:37:44 -0600

    add Oauth

--- stick_to_it/main_app/models.py
+++ stick_to_it/main_app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+from django.contrib.auth.models import User
 # Create your models here.
 class Card(models.Model):
     activity = models.CharField(max_length=100)
     due_date = models.DateField(blank=True)
     complete_date = models.DateField(blank=True)
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     def get_absolute_url(self):
         return reverse('index')
--- stick_to_it/stick_to_it/settings.py
+++ stick_to_it/stick_to_it/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Add this variable to specify where successful logins should redirect to
+LOGIN_REDIRECT_URL = '/cards/'
+
+# Add this variable to specify where logging out redirects to
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
