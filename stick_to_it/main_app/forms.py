c36a40dee725f69f49206f7e0ebdc8c97f18b1d8
mlsmoorman <melissa.s.moorman@gmail.com>
2023-12-18 17:17:57 -0600

    fixes data picker

---
+++ stick_to_it/main_app/forms.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm, Textarea
+from django import forms
+from .models import Card
+
+class CardForm(ModelForm):
+    class Meta:
+        model = Card
+        fields = ['activity', 'due_date']
+        # widgets = {
+		# 	'due_date': Textarea(attrs={'type':'date'})
+		# }
+        # due_date = forms.DateField(widget=forms.widgets.DateInput(attrs={'type':'date'})
+        widgets = {
+            'due_date': forms.DateInput(attrs={'type': 'date'})
+        }
--- stick_to_it/main_app/models.py
+++ stick_to_it/main_app/models.py
@@ -1,12 +1,17 @@
+from django.contrib import admin
 from django.db import models
 from django.urls import reverse
+from django import forms
 
 from django.contrib.auth.models import User
+
 # Create your models here.
 class Card(models.Model):
     activity = models.CharField(max_length=100)
-    due_date = models.DateField(blank=True)
-    complete_date = models.DateField(blank=True)
+    due_date = models.DateField(blank=True, null=True)
+    complete_date = models.DateField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     def get_absolute_url(self):
         return reverse('index')
+    
