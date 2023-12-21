51dc6815fce4b9fb495532f688aa562f1a2feee1
mlsmoorman <melissa.s.moorman@gmail.com>
2023-12-20 20:13:58 -0600

    cleans up code and updates readme

--- stick_to_it/main_app/forms.py
+++ stick_to_it/main_app/forms.py
@@ -6,9 +6,12 @@ class CardForm(ModelForm):
     class Meta:
         model = Card
         fields = ['activity', 'due_date', 'complete_date']
+        # widgets are used to make an Admin Model form
         widgets = {
+            # the DateInput widget changes the django 'type' from text to date creating a Date Picker
             'due_date': forms.DateInput(attrs={'type': 'date'}),
             'complete_date': forms.DateInput(attrs={'type': 'date'}),
+            # the TextArea widget changes the input from text to a TextArea allowing user to see all of their inputs instead of having to scroll
             'activity': forms.Textarea(attrs={'rows':5, 'cols':16})
         }
        
